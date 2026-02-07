d5efea85eb765cdb792b9a7289b4e3ffce1c868d
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2026-02-07 18:17:00 +0530

    razorpay updated cde

--- src/Config.jsx
+++ src/Config.jsx
@@ -2,10 +2,10 @@
 // export const API_URL = "http://192.168.29.78:5000";
 
 // export const API_URL = "https://portal.aryu.agency";
-export const API_URL = "https://airesumebuilder.aryuacademy.com";
+// export const API_URL = "https://airesumebuilder.aryuacademy.com";
 
-// export const API_URL = "http://localhost:5000";
-
+export const API_URL = "http://localhost:3015";
+export const RAZORPAY_KEY_ID = "rzp_live_S63MYeJCw1dvz8";
 // export const API_URL = "http://192.168.0.152:3003";  
 
 
